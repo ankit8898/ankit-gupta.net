6fe078c4c15272731b136fbe024ca98c04a1c492
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-03-03 21:54:23 -0500

    Photography Link and some fancy stuff

---
+++ app/assets/javascripts/vibrate.js
@@ -0,0 +1,44 @@
+$(function() {
+jQuery.fn.vibrate = function (conf) {
+    var config = jQuery.extend({
+        speed:        30, 
+        duration:    1000,  
+        spread:        3
+    }, conf);
+
+    return this.each(function () {
+        var t = jQuery(this);
+
+        var vibrate = function () {
+            var topPos    = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+            var leftPos    = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+            var rotate    = Math.floor(Math.random() * config.spread) - ((config.spread - 1) / 2);
+
+            t.css({
+                position:            'relative', 
+                left:                leftPos + 'px', 
+                top:                topPos + 'px', 
+                WebkitTransform:    'rotate(' + rotate + 'deg)'  // cheers to erik@birdy.nu for the rotation-idea
+            });
+        };
+
+        var doVibration = function () {
+            var vibrationInterval = setInterval(vibrate, config.speed);
+
+            var stopVibration = function () {
+                clearInterval(vibrationInterval);
+                t.css({
+                    position:            'static', 
+                    WebkitTransform:    'rotate(0deg)'
+                });
+            };
+
+            setTimeout(stopVibration, config.duration);
+        };
+        doVibration();
+    });
+};
+$('#heart').hover(function() {$(this).vibrate();});
+
+});
+
